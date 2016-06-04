fe31682b6c28409543094f79016a9b6c2476ac3b
Alpesh Khambhala <alpesh.ror@gmail.com>
2016-06-04 15:21:45 +0530

    fixed contact page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,12 +4,18 @@ class HomeController < ApplicationController
 
   def contact
     contact = contact_params
+    if current_user
+      contact[:name] = current_user.full_name
+      contact[:email] = current_user.email
+      contact[:phone] = current_user.phone_number
+      contact[:city] = current_user.addresses.first.try(:city)
+    end
     UserMailer.contact_mail(contact).deliver_now
     render json: { msg: "success" }
   end
 
   private
     def contact_params
-      params.require(:contact).permit(:name, :email, :city, :phone, :message)
+      params.require(:contact).permit(:user_id, :name, :email, :city, :phone, :message)
     end
 end
