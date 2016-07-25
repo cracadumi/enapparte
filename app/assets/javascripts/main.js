07c23e6615a9c79a0c570c8dd5110393d6a47422
Mahesh <mahesh@techcompose.com>
2016-07-25 17:18:34 +0530

    fixed issue #119 - Resolved issue of auto close on clicking of time dropdown

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,4 +7,20 @@ $(document).ready( function(){
   $('.modal').on('show.bs.modal', function() {
     $('.flash-messages .alert').alert('close');
   });
+  $(document).on('focus','.dropdown input',function(){
+  	if($(this).parent().hasClass('open')){
+  		$(this).parent().removeClass('open');
+  	}
+  	else{
+  		$(this).parent().addClass('open');	
+  	}
+  });
+  	$('body').on('click', function (e) {
+	    if (!$('div.dropdown').is(e.target) 
+	        && $('div.dropdown').has(e.target).length === 0 
+	        && $('div.open').has(e.target).length === 0
+	    ) {
+	        $('div.dropdown').removeClass('open');
+	    }
+	});
 });
