bc514e1b917f206c4c58f505f68ca4bca5f1d8ad
Dipak <dipak@techcompose.com>
2016-07-28 15:16:15 +0530

    Bug fixed - issue #160 - fix bugs when saving new booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -11,9 +11,17 @@ class Booking < ActiveRecord::Base
   just_define_datetime_picker :paid_on
   just_define_datetime_picker :paid_out_on
 
+  before_save :set_payout_value
+
   alias_method :name, :id
   
   enum status: { confirmed: 1, pending: 2, canceled: 3, expired: 4 }
+
+  def set_payout_value
+    if price.present?
+      self.payout = (price/ENV['COMMISSION'].to_f).round(2)
+    end
+  end
 
   def change_status status
     if self.update status: status
